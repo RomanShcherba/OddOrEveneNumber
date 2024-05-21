c6ad68951c0730ed2519220fb0524fe9bd1affb1
warbo <warbo@PC370>
2024-05-21 11:25:06 +0300

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+
+            string[] message = { "Even number.", "Odd number." };
+
+            Console.WriteLine(message[number & 1]);
+        }
+    }
+}
