df8e33fb7bcbd0ad59cdb5b50ab5b4162bf2ee4a
HellHounD668 <92847483+RomanShcherba@users.noreply.github.com>
2024-05-23 15:27:00 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,10 +13,10 @@ namespace ConsoleApp1
             Console.Write("Enter a number:");
             int number;
             while (!int.TryParse(Console.ReadLine(), out number))
-                {
-                    Console.WriteLine("Invalid input. Please enter integer.");
-                    Console.Write("Enter a number: ");
-                }
+             {
+               Console.WriteLine("Invalid input. Please enter integer.");
+               Console.Write("Enter a number: ");
+             }
             string[] message = { "Even number.", "Odd number." };
             Console.WriteLine(message[number & 1]);
         }
